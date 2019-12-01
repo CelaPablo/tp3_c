add6fdb3882b6c3558c37d98ba7b42ec50f250f3
Pablo <pablooandress9@gmail.com>
2019-11-30 21:32:19 -0300

    algo del 4

--- Ejercicio4/consultor.c
+++ Ejercicio4/consultor.c
@@ -18,12 +18,9 @@
 #include <semaphore.h>
 #include <sys/mman.h>
 
-int validarParametros(int arg, char *args[])
-{
+int validarParametros(int arg, char *args[]) {
     if (arg != 2)
-    {
         return 1;
-    }
 
     return 0;
 }
@@ -35,40 +32,45 @@ void mostrarAyuda() {
     printf("\t ./consumir ./articulos.txt \n");
     printf("\n Ejemplo de ejecucion consultor:\n");
     printf("\t ./consultar producto=P.DULCE \n");
-    return;
+    exit(0);
 }
-int main(int arg, char *args[])
-{
+
+int main(int arg, char *args[]) {
 
     char *entrada = args[1];
-
-    int tam = sizeof(char) * 30;
+    int tam = sizeof(char) * 100;
 
     if (arg == 2 && (strcmp(args[1], "-h") == 0 || strcmp(args[1], "-help") == 0 || strcmp(args[1], "-?") == 0))
-    {
         mostrarAyuda();
-        return 0;
-    }
-    // if (validarParametros(arg, args) == 1)
-    //   return 1;
-    int fd;
-    char *mem;
-    sem_t *sem = sem_open("semaforo", O_CREAT, 0600, 0);                      // crear semaforo
-    sem_t *sem2 = sem_open("semaforo2", O_CREAT, 0600, 0);                    // crea segundo semaforo
-    fd = shm_open("/nombre1", O_CREAT | O_RDWR, 0600);                        // crear memoria compartida
-    ftruncate(fd, tam);                                                       // definir tamaño
-    mem = (char *)mmap(NULL, tam, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); // asociar espacio a variable
-    strcpy(mem, entrada); // escribo la consulta en la memoria compartida
-    // una vez que envio el mensaje y recibo la confirmacion cierro todo
-    sem_post(sem);  // V(); // libero el semaforo para que el proceso "ejercicio3" pueda leer de la memoria
-    sem_wait(sem2); //P() // me quedo bloqueado hasta que se lea de la memoria
-    close(fd);
-    munmap(mem, tam); // elino la asociacion de la variable con la memoria
-    shm_unlink("/nombre1");// elimino la memoria compartida
-    sem_close(sem);
-    sem_unlink("semaforo"); // elimino el semaforo
-    sem_close(sem2);
-    sem_unlink("semaforo2");
 
-    return 0;
+    printf("antes de los semaforos\n");
+
+    sem_t *sem1 = sem_open("semaforo1", O_CREAT); // para que se bloquee hasta que llegue una consulta
+    sem_t *sem2 = sem_open("semaforo2", O_CREAT); // para bloquear cuando arranca consultor
+    sem_t *sem3 = sem_open("semaforo3", O_CREAT); // porq si
+
+    sem_wait(sem2);
+    printf("espera\n");
+
+    int fd;
+    char *memC;                 // crea segundo semaforo
+    fd = shm_open("/consultas", O_CREAT | O_RDWR, 0600);                        // crear memoria compartida
+    ftruncate(fd, tam);                                                       // definir tamaño
+    memC = (char *)mmap(NULL, tam, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); // asociar espacio a variable
+    strcpy(memC, entrada); // escribo la consulta en la memoria compartida
+
+    sem_post(sem1);
+    exit(0);
+    // una vez que envio el mensaje y recibo la confirmacion cierro todo
+    // sem_post(sem);  // V(); // libero el semaforo para que el proceso "ejercicio3" pueda leer de la memoria
+    // sem_wait(sem2); //P() // me quedo bloqueado hasta que se lea de la memoria
+    // close(fd);
+    // munmap(mem, tam); // elino la asociacion de la variable con la memoria
+    // shm_unlink("/nombre1");// elimino la memoria compartida
+    // sem_close(sem);
+    // sem_unlink("semaforo"); // elimino el semaforo
+    // sem_close(sem2);
+    // sem_unlink("semaforo2");
+    //
+    // return 0;
 }
--- Ejercicio4/ejercicio4.c
+++ Ejercicio4/ejercicio4.c
@@ -19,48 +19,49 @@
 #include <semaphore.h>
 #include <sys/mman.h>
 
-int validarParametros(int arg, char *args[])
-{
+void validarParametros(int arg, char *args[]) {
 
-    if (arg != 2) {
-        printf("\nCANTIDAD DE PARAMETROS INCORRECTOS,VERIFIQUE LA AYUDA\n");
-        return 1;
-    }
+  if (arg != 2) {
+      printf("\nCANTIDAD DE PARAMETROS INCORRECTOS,VERIFIQUE LA AYUDA ( ./ejercicio4 -h )\n");
+      exit(1);
+  }
 
-    struct stat myFile;
-    if (stat(args[1], &myFile) < 0) {
-        printf("\nNo se encontro el archivo %s\n", args[1]);
-        return 1;
-    }
+  struct stat myFile;
+  if (stat(args[1], &myFile) < 0) {
+      printf("\nNo se encontro el archivo %s\n", args[1]);
+      exit(1);
+  }
 
-    return 0;
+  return;
 }
 
 void mostrarAyuda() {
-    printf("\n Ejemplo de ejecucion:\n");
-    printf("\t ./ejercicio4 ./articulos.txt  \n");
-    printf("\n Ejemplo de ejecucion consumidor:\n");
-    printf("\t ./consumir ./articulos.txt \n");
-    printf("\n Ejemplo de ejecucion consultor:\n");
-    printf("\t ./consultar producto=P.DULCE \n");
-    return;
+  printf("\n Ejemplo de ejecucion:\n");
+  printf("\t ./ejercicio4 ./articulos.txt  \n");
+  printf("\n Ejemplo de ejecucion consumidor:\n");
+  printf("\t ./consumir ./articulos.txt \n");
+  printf("\n Ejemplo de ejecucion consultor:\n");
+  printf("\t ./consultar producto=P.DULCE \n");
+  exit(0);
 }
 
-void agregarSalida(char out[], char id[], char articulo[], char producto[], char marca[])
-{
-    strcat(out, id);
-    strcat(out, ";");
-    strcat(out, articulo);
-    strcat(out, ";");
-    strcat(out, producto);
-    strcat(out, ";");
-    strcat(out, marca);
-    strcat(out, "\n");
-    return;
+void agregarSalida(char out[], char id[], char articulo[], char producto[], char marca[]) {
+  strcat(out, id);
+  strcat(out, ";");
+  strcat(out, articulo);
+  strcat(out, ";");
+  strcat(out, producto);
+  strcat(out, ";");
+  strcat(out, marca);
+  strcat(out, "\n");
+  return;
 }
 
-int obtenerCantidadDeRegistros(char *path[])
-{
+void agregarSalidaNula(char out[]) {
+  strcat(out, "\n No se encontraron resultados \n");
+}
+
+int obtenerCantidadDeRegistros(char *path[]) {
     FILE *pf;
     int cantfilas = 0;
     pf = fopen(*path, "r");
@@ -78,19 +79,15 @@ int obtenerCantidadDeRegistros(char *path[])
     return cantfilas;
 }
 
-void filtrarArchivo(char *path[], char *filtro, int registros, char *salida)
-{
+void filtrarArchivo(char *path[], char *filtro, int registros, char *salida) {
 
     FILE *pf;
     int esId = strncmp("ID", filtro, 2);
     int esProducto = strncmp("PRODUCTO", filtro, 8);
     int esMarca = strncmp("MARCA", filtro, 5);
-    // printf("esMarca: %d",esMarca);
-    int j = 0;
-    printf("valor j=%d\n", j);
+    int agregados = 0;
     char id[100];
     char articulo[100];
-
     char producto[100];
     char marca[100];
     char *igual = strchr(filtro, '=');
@@ -98,25 +95,20 @@ void filtrarArchivo(char *path[], char *filtro, int registros, char *salida)
 
     strcpy(salida, " ");
     buscado++;
-    printf("buscado: %s\n", buscado);
 
-    printf("\nFILTRO: %s\n", filtro);
     pf = fopen(*path, "r");
-    if (!pf)
-    {
-
+    if (!pf) {
         printf("\nNo se  encontro el archivo %s\n", *path);
         exit(0);
     }
+
     strcpy(id, " ");
     strcpy(articulo, " ");
     strcpy(producto, " ");
     strcpy(marca, " ");
 
-    while (!feof(pf))
-    {
+    while (!feof(pf)) {
 
-        j++;
         fflush(stdin);
         fscanf(pf, " %[^;]", id);
 
@@ -130,20 +122,15 @@ void filtrarArchivo(char *path[], char *filtro, int registros, char *salida)
         fscanf(pf, " ;%[^\r|\n]", marca);
         printf("id: %s articulo: %s producto:%s marca: %s\n", id, articulo, producto, marca);
 
-        if (esId == 0 && strcmp(id, buscado) == 0)
-        {
-
-            agregarSalida(salida, id, articulo, producto, marca);
-        }
-        else if (esMarca == 0 && strcmp(marca, buscado) == 0)
-        {
-
-            agregarSalida(salida, id, articulo, producto, marca);
-        }
-        else if (esProducto == 0 && strcmp(producto, buscado) == 0)
-        {
-
-            agregarSalida(salida, id, articulo, producto, marca);
+        if (esId == 0 && strcmp(id, buscado) == 0) {
+          agregados++;
+          agregarSalida(salida, id, articulo, producto, marca);
+        } else if (esMarca == 0 && strcmp(marca, buscado) == 0) {
+          agregados++;
+          agregarSalida(salida, id, articulo, producto, marca);
+        } else if (esProducto == 0 && strcmp(producto, buscado) == 0) {
+          agregados++;
+          agregarSalida(salida, id, articulo, producto, marca);
         }
 
         strcpy(id, " ");
@@ -151,87 +138,97 @@ void filtrarArchivo(char *path[], char *filtro, int registros, char *salida)
         strcpy(producto, " ");
         strcpy(marca, " ");
     }
+
+    if(agregados == 0)
+      agregarSalidaNula(salida);
+
     fclose(pf);
 
     return;
 }
 
-int main(int arg, char *args[])
-{
-    int x;
-    int num = arg;
+int main(int arg, char *args[]) {
 
     if (arg == 2 && (strcmp(args[1], "-h") == 0 || strcmp(args[1], "-?") == 0 || strcmp(args[1], "-help") == 0))
-    {
-
         mostrarAyuda();
 
-        return 0;
-    }
-    int res = validarParametros(arg, args);
-    if (res == 1)
-    {
-        return 0;
-    }
+    validarParametros(arg, args);
 
+    /*int pid = fork(); // creo demonio
+    if(pid > 0) {
+      printf("kill %d\n", pid);
+      exit(0);
+    }*/
+
+    int registros = obtenerCantidadDeRegistros(&args[1]);
+
+    // Crear semaforos
+    sem_t *sem1 = sem_open("semaforo1", O_CREAT, 0600, 0); // para que se bloquee hasta que llegue una consulta
+    sem_t *sem2 = sem_open("semaforo2", O_CREAT, 0600, 0); // para bloquear cuando arranca consultor
+    sem_t *sem3 = sem_open("semaforo3", O_CREAT, 0600, 0); // porq si
+
+    // crear memoria compartida para consulta
+    int fd;
+    char *memC;
+    size_t tam = sizeof(char) * 100;
+    fd = shm_open("/consultas", O_CREAT | O_RDWR, 0600);
+    memC = (char *)mmap(NULL, tam, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    // crear memoria compartida para consulta
+    int fd2;
+    char *memR;
+    size_t tam2 = sizeof(char) * 100;
+    fd2 = shm_open("/resultados", O_CREAT | O_RDWR, 0600);
+    memR = (char *)mmap(NULL, tam2, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    printf("andtes del while\n");
     //si es el hijo se queda ejecutando
-    while (1)
-    {
+    while (1) {
+      char filtro[100];
+      printf("antes de la espera\n");
+      sem_wait(sem1); // P() // me quedo bloquedo hasta poder leer de la memoria
+      printf("despues de la espera");
+      strcpy(filtro, memC);
+      printf("%s\n", filtro);
 
-        char filtro[100];
-        int registros = obtenerCantidadDeRegistros(&args[1]);
-
-       // **** MEMORIA COMPARTIDA PARA RECIBIR CONSULTA********
-        int fd;
-        char *mem;
-        sem_t *sem = sem_open("semaforo", O_CREAT, 0600, 0); // crear semaforo
-        sem_t *sem2 = sem_open("semaforo2", O_CREAT, 0600, 0);
-        size_t tam = sizeof(char) * 100;
-        fd = shm_open("/nombre1", O_CREAT | O_RDWR, 0600);                        // crear memoria compartida
-        mem = (char *)mmap(NULL, tam, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); // asociar espacio a variable
-        sem_wait(sem); // P() // me quedo bloquedo hasta poder leer de la memoria
-        printf("%s\n", mem);
-        strcpy(filtro, mem);
-        sem_post(sem2); // V()
-        close(fd);
-        munmap(mem, tam);
-        sem_close(sem);
-        sem_close(sem2);
-
-        char salida[registros * 40];       
-        int cantCaracteres = strlen(filtro);
-        char *aMayuscula = filtro;
-
-        // pongo en mayuscula el filtro
-        for (int i = 0; i < cantCaracteres; i++)
-        {
-            *aMayuscula = toupper(*aMayuscula);
-            aMayuscula++;
-        }
-
-        filtrarArchivo(&args[1], filtro, registros, salida);
-
-        printf("\n******ARCHIVO FILTRADO*******\n");
-        ///**** MEMORIA PARA ENVIAR EL RESULTADO DE LA CONSULTA******
-        int fds;
-        char *mems;
-        sem_t *sem3 = sem_open("semaforo3", O_CREAT, 0600, 0); // crear semaforo
-        sem_t *sem4 = sem_open("semaforo4", O_CREAT, 0600, 0); // crea segundo semaforo
-        size_t tam2 = sizeof(char) * 30 * registros;
-        fds = shm_open("/mc2", O_CREAT | O_RDWR, 0600);                              // crear memoria compartida
-        ftruncate(fds, tam2);                                                        // definir tamaño
-        mems = (char *)mmap(NULL, tam2, PROT_READ | PROT_WRITE, MAP_SHARED, fds, 0); // asociar espacio a variable
-        strcpy(mems, salida);                                                        // copio lo que tenga salida a la memoria compartida;
-        sem_post(sem3);                                                              // V();
-        sem_wait(sem4);                                                              //P()
-        close(fds);
-        munmap(mems, tam2);
-        shm_unlink("/mc2");
-        sem_close(sem3);
-        sem_unlink("semaforo3");
-        sem_close(sem4);
-        sem_unlink("semaforo4");
+      exit(0);
+  //     sem_post(sem2); // V()
+  //     close(fd);
+  //     munmap(mem, tam);
+  //     sem_close(sem);
+  //     sem_close(sem2);
+  //
+  //     char salida[registros * 40];
+  //     int cantCaracteres = strlen(filtro);
+  //     char *aMayuscula = filtro;
+  //
+  //     // pongo en mayuscula el filtro
+  //     for (int i = 0; i < cantCaracteres; i++) {
+  //       *aMayuscula = toupper(*aMayuscula);
+  //       aMayuscula++;
+  //     }
+  //
+  //     filtrarArchivo(&args[1], filtro, registros, salida);
+  //
+  //     int fds;
+  //     char *mems;
+  //     sem_t *sem3 = sem_open("semaforo3", O_CREAT, 0600, 0); // crear semaforo
+  //     sem_t *sem4 = sem_open("semaforo4", O_CREAT, 0600, 0); // crea segundo semaforo
+  //     size_t tam2 = sizeof(char) * 30 * registros;
+  //     fds = shm_open("/mc2", O_CREAT | O_RDWR, 0600);                              // crear memoria compartida
+  //     ftruncate(fds, tam2);                                                        // definir tamaño
+  //     mems = (char *)mmap(NULL, tam2, PROT_READ | PROT_WRITE, MAP_SHARED, fds, 0); // asociar espacio a variable
+  //     strcpy(mems, salida);                                                        // copio lo que tenga salida a la memoria compartida;
+  //     sem_post(sem3);                                                              // V();
+  //     sem_wait(sem4);                                                              //P()
+  //     close(fds);
+  //     munmap(mems, tam2);
+  //     shm_unlink("/mc2");
+  //     sem_close(sem3);
+  //     sem_unlink("semaforo3");
+  //     sem_close(sem4);
+  //     sem_unlink("semaforo4");
     }
-
-    return (0);
+  //
+  // return (0);
 }
