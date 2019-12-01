9b51e80836b910b22e03e0c6f3ecdd0e2e046f19
Agustín Eduardo Pessolani <31143916+APessolani@users.noreply.github.com>
2019-12-01 15:56:24 -0300

    Update ejercicio1.c

--- Ejercicio1/ejercicio1.c
+++ Ejercicio1/ejercicio1.c
@@ -1,304 +1,130 @@
-/***********************************
- Trabajo práctico N3 Ejercicio 1
- Integrantes:
- Cabral, David           39757782
- Cela, Pablo             36166857
- Pessolani, Agustin      39670584
- Sullca, Fernando        37841788
- Zabala, Gaston          34614948
- ***********************************/
+/*********************************** 
+Trabajo práctico N3 ejercicio 1
+Script: ejercicio1.c
+Integrantes:
+Cabral, David           39757782
+Cela, Pablo             36166857
+Pessolani, Agustin	    39670584
+Zabala, Gaston          34614948 
+***********************************/
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
+#include <signal.h>
 
-#define TIPO(y) ((y == 'Z') ? "Zombie" : (y == 'D') ? "Demonio" : ("Normal"))
-#define PARENTESCO(Y) ((Y==2)?"hijo": (Y==3) ? "nieto" : "bisnieto")
+void ayuda(char *argv[]);
+void padre_hijo(int);
 
-int generacion = 0;        // generación del proceso
-int cantidad_hijos = 2;    // cantidad de hijos que tiene el proceso
-int cantidad_zombies = 2;  // cantidad de zombies que tiene el proceso
-int cantidad_demonios = 0; // cantidad de demonios que tiene el proceso
-int numero_hermanos = 1;   // cantidad de hermanos que son en total en esta generación(1,2,4,8)
-int ppid = -1;             // ppid del proceso que muestra sus datos
-int entro = 0;              //Si debo crear los zombies de manera aleatoria
+pid_t pid_hijo[2];
+pid_t pid_nieto[3];
+pid_t pid_bisnieto[5];
 
-// mostrar datos del proceso en curso
-void mostrarDatoProceso(char tipo);
+int main(int argc, char *argv[]){
+    int cant_hijos = 2;
+    int i,j,k;
+    signal(SIGUSR1,padre_hijo);
+    if(argc == 2){
+        ayuda(argv);
+    }
+    else if(argc > 2){
+        printf("Ingreso demasiados parámetros.\n");
+        exit(2);
+    }
+    
 
-// crear procesos hijos
-void crearHijos();
-
-// crear procesos demonios (solo pueden ser hijos de zombies)
-// Retorna:
-//  0: si es el proceso demonio creado
-//  mayor a 0: si es el proceso que creó los demonios
-int crearDemonios();
-
-void crearZombies();
-
-void ayuda();
-
-int main(int arg,char** arg2 )
-{
-    printf("Para cerrar cada proceso se debe presionar ENTER  \n");
-
-    /***Verfficacion********/
-
-    if(arg>1)
-    {
-        if (strcmp(arg2[1],"help") || strcmp(arg2[1],"h")|| strcmp(arg2[1],"?"))
-        {
-            ayuda();
-            return 0;
-        }
-        else
-        {
-            printf("OPTIONS DE AYUDA\n");
-            printf("\t --help \t -h \t -?: muestra La  ayuda y discripcion del ejercicio\n");
-            printf("EXAMPLES: muestra esta ayuda\n");
-            printf("\t./ejercicio1 -h\n");
-            printf("LA EJECUCCION DEL PROCESOS NO NECESITA PARAMETROS\n");
-            return 0;
+    printf("PID: %d PPID: %d Parentesco-Tipo: Padre\n",getpid(),getppid());
+    for(i = 0; i < 2; i++){
+        pid_hijo[i] = fork();
+        if(pid_hijo[i] == 0){
+            printf("PID: %d PPID: %d Parentesco-Tipo: [hijo]-[normal]\n",getpid(),getppid());
+            //HIJO 1
+            if(i == 0){
+                for(j = 0; j < 2; j++){
+                    pid_nieto[j] = fork();
+                    //NIETO 1 Y 2
+                    if(pid_nieto[j] == 0){
+                        printf("PID: %d PPID: %d Parentesco-Tipo: [nieto]-[normal]\n",getpid(),getppid());
+                        //BISNIETO 1 Y 2 -- ZOMBIES
+                        if(j == 0){
+                            for(k = 0; k < 2; k++){
+                                pid_bisnieto[k] = fork();
+                                if(pid_bisnieto[k] == 0){
+                                    printf("PID: %d PPID: %d Parentesco-Tipo: [bisnieto]-[zombie]\n",getpid(),getppid());
+                                    while (1){
+                                        pause();
+                                        exit(0);
+                                    }   
+                                }
+                            }
+                        }
+                        //BISNIETO 3 Y 4 -- DEMONIOS
+                        if(j == 1){
+                            for(k = 2; k < 4; k++){
+                                pid_bisnieto[k] = fork();
+                                if(pid_bisnieto[k] == 0){
+                                    printf("PID: %d PPID: %d Parentesco-Tipo: [bisnieto]-[demonio]\n",getpid(),getppid());
+                                    while (1){}
+                                }
+                                //printf("pid [%d], k %d\n",pid_bisnieto[k],k);
+                            }
+                        }
+                        pause();
+                        if(j == 0){
+                            sleep(15);
+                            kill(pid_bisnieto[0],SIGUSR1);
+                            kill(pid_bisnieto[1],SIGUSR1);
+                        }
+                        exit(2);
+                        while (1){}
+                    }
+                }
+            }
+            //HIJO 2
+            else if (i == 1){
+                j = 2;
+                pid_nieto[j] = fork();
+                //NIETO 3
+                if(pid_nieto[j] == 0){
+                    printf("PID: %d PPID: %d Parentesco-Tipo: [nieto]-[normal]\n",getpid(),getppid());
+                    k = 4;
+                    pid_bisnieto[k] = fork();
+                    //BISNIETO 5 -- DEMONIO
+                    if(pid_bisnieto[k] == 0){
+                        printf("PID: %d PPID: %d Parentesco-Tipo: [bisnieto]-[demonio]\n",getpid(),getppid());
+                        while (1){}
+                    }
+                    //printf("pid [%d], k %d\n",pid_bisnieto[k],k);
+                    while (1){}
+                }
+            }
+            pause();
+            if(i == 0){
+                kill(pid_nieto[0],SIGUSR1);
+                kill(pid_nieto[1],SIGKILL);
+            }
+            else
+                kill(pid_nieto[2],SIGKILL);
+            exit(2);    
         }
     }
-    /*******Fin de Verificacion****/
-    srand(time(NULL));
-
-    //printf("PID: %d PPID: %d Proceso Padre \n",getpid(),getppid()); No se pudo dividir...
-    mostrarDatoProceso('G');
-
-    // iniciar procreación
-    crearHijos();
-
-    // esperar una tecla (se ejecuta en todos los hijos ) y mantenerlos en memoria , poder usar "ps" o "pstree"
     getchar();
-    printf("proceso [%d] finalizado\n", getpid());
-
+    kill(pid_hijo[0],SIGUSR1);
+    kill(pid_hijo[1],SIGUSR1);
+    sleep(10);
     return 0;
 }
 
-void mostrarDatoProceso(char tipo)
-{
-    // siempre se muestran los procesos apenas son creados
-    // por lo que podemos aumentar la generación antes de mostrarlos
-    generacion++;
-
-    // si es un demonio
-    if (tipo == 'D')
-    {
-        // esperamos a que muera su padre (que se haga zombie)
-        // y luego mostramos sus datos
-        // para que el ppid del demonio se muestre bien
-        while (ppid == getppid())
-        {
-        }
-
+void ayuda(char *argv[]){
+    if(strcmp(argv[1],"-h") == 0 || strcmp(argv[1],"-?") == 0 || strcmp(argv[1],"-help") == 0){
+        printf("El programa se encarga de crear una jerarquía familiar:\n");
+        printf("1 padre, 2 hijos, 3 nietos y 5 bisnietos\n.Además cada padre solo puede tener 2 hijos como máximo.\n");
+        printf("Dos procesos serán zombies y 3 serán demonios.\n");
+        printf("No recibe ningún parámetro.\n");
+        exit(1);
     }
-
-    if(generacion==1)
-    {
-        printf("PID: %d PPID: %d Padre \n",getpid(),getppid());
-    }
-    else
-        printf("PID: %d PPID: %d Parentesco-Tipo: [%s]-[%s]\n",
-               getpid(),getppid(),PARENTESCO(generacion),TIPO(tipo));
 }
 
-void crearHijos()
-{
-    int hijo_pid;
-
-    numero_hermanos = cantidad_hijos * generacion- 1;
-
-    if(entro>0)
-    cantidad_zombies = 0;
-    /**********Procesos de crear hijos***************/
-    while (cantidad_hijos)
-    {
-        // crear nuevo hijo
-        hijo_pid = fork();
-
-        // si estamos en el proceso hijo
-        if (hijo_pid == 0)
-        {
-
-            mostrarDatoProceso('G');
-
-            // solamente el primer proceso tiene hijos zombie(cualqueir nivel)
-            if(entro>0)
-                cantidad_zombies=0;
-
-            // si aún no es la última generación
-            if (generacion < 4)
-            {
-                // la cantidad de hijos que tiene, depende del número de
-                // hermanos que tenga el proceso
-                cantidad_hijos = (cantidad_hijos == numero_hermanos)? 1 : 2;
-
-                /// crear hijos del proceso hijo ,Zombies /demonios
-                /*******Valor aleatorio para zombies en diferentes niveles****/
-
-                if(entro<1)
-                {
-                    int c=rand()%2;
-                    /// printf("%d valor random,\n",c);
-
-                    if(c==1 && cantidad_zombies>=1)
-                    {
-                        ///  printf("Creando zombies\n");
-                        ///  printf("%d entro\n",entro);
-                        entro++;
-                /****************Zona Proceso Zombie*************************/
-                        while (cantidad_zombies)
-                        {
-                            // crear nuevo zombie
-                            hijo_pid = fork();
-
-                            // si es el proceso hijo
-                            if (hijo_pid == 0)
-                            {
-
-                                mostrarDatoProceso('Z');
-
-                                /** el primer zombie tiene 2 demonios
-                                 el segundo zombie tiene 1 demonio
-                                 Total 3 demonios
-                                */
-                                cantidad_demonios = cantidad_zombies;
-
-                                // si es el proceso demonio
-                                if (crearDemonios() == 0)
-                                {
-                                    // salimos del while que fue creado en su padre
-                                break;
-                                }
-                                // finalizar proceso para que sea zombie
-                                exit(0);
-                            }
-
-                            // indicar que el zombie fue creado y crear el siguiente si lo hay
-                            cantidad_zombies--;
-                        }
-                    }
-                }
-                /*******************FIN de Zombie******************************************************/
-                crearHijos();
-            }
-
-            // como es el hijo, salimos del while
-            // porque el while fue creado en el padre
-            break;
-        }
-        /**************************FIN HIJO******************/
-
-        // indicar que el hijo fue creado y crear el siguiente si lo hay
-        cantidad_hijos--;
-
-    }
-
-    /**********Procesos de Padre ***************/
-
-/// crearZombies(); Los crea para el primero unicamente
-/***
-
-if(entro==0 && cantidad_zombies>0 && generacion <4)
-crearZombies();***/
-
-//printf("%d %d\n",cantidad_demonios,cantidad_zombies);
-
-/**if(cantidad_zombies>0 && entro ==0)
-    crearZombies();
-*/
-}
-
-void crearZombies()
-{
-    int hijo_pid;
-// crear zombies
-    while (cantidad_zombies)
-    {
-        // crear nuevo zombie
-        hijo_pid = fork();
-
-        // si es el proceso hijo
-        if (hijo_pid == 0)
-        {
-            // mostrar datos del proceso zombie
-            mostrarDatoProceso('Z');
-
-            // si es zombie, puede tener hijos demonio
-            // el primer zombie tiene 2 demonios
-            // el segundo zombie tiene 1 demonio
-
-            cantidad_demonios = cantidad_zombies;
-
-            // si es el proceso demonio
-            if (crearDemonios() == 0)
-            {
-                // salimos del while que fue creado en su padre
-                break;
-            }
-
-            // finalizar proceso para que sea zombie
-            exit(0);
-        }
-
-        // indicar que el zombie fue creado y crear el siguiente si lo hay
-        cantidad_zombies--;
-    }
-
-}
-
-int crearDemonios()
-{
-    int hijo_pid;
-
-    // los demonios muestran sus datos, luego de que cambie su ppid
-    ppid = getpid();
-
-    while (cantidad_demonios)
-    {
-        // crear nuevo demonio
-        hijo_pid = fork();
-
-        // si estamos dentro del proceso demonio
-        if (hijo_pid == 0)
-        {
-            // mostrar datos del proceso demonio
-            mostrarDatoProceso('D');
-
-            while (1)
-            {
-                // mantener proceso demonio vivo
-            }
-            // salir del while porque fue creado
-            // para el proceso padre
-            return 0;
-        }
-        // indicar que el demonio fue creado y crear el siguiente si lo hay
-        cantidad_demonios--;
-    }
-
-    return getpid();
-}
-
-void ayuda()
-{
-    printf("NAME\n");
-    printf("\tejercicio1\n");
-    printf("DESCRIPTION\n");
-    printf("\tCreacion de procesos hijos en diferente niveles y tipos de procesos(Normales ,demonios,zombies)\n");
-    printf(" NOTA:\n Los zombie finalizan junto con el padre \n Los demonios quedan corriendo  \n");
-    printf("SYNOPSIS\n");
-    printf("\t./ejercicio1 OPTION\n");
-    printf("OPTIONS\n");
-    printf("\t --help: muestra esta ayuda\n");
-    printf("\t (Sin opciones): muestra los datos de los procesos generados\n");
-    printf("EXAMPLES:\n");
-    printf("\t./ejercicio1 -h\n : Ejecucion de ayuda");
-    printf("\t./ejercicio1 : Ejecucion normal\n");
-
-}
+void padre_hijo(int sig){}
