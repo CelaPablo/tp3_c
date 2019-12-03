a02d306ad76f870605ef36b14a9f17444fe626ec
Agustín Eduardo Pessolani <31143916+APessolani@users.noreply.github.com>
2019-12-03 14:13:32 -0300

    version final

--- ejercicio5/cliente.c
+++ ejercicio5/cliente.c
@@ -15,39 +15,80 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-
-int validarParametros(int arg, char *args[]){
-    if (arg != 2){
-        return 1;
+void mostrarAyuda(int arg, char *args[]){
+    if (arg == 2 && (strcmp(args[1], "-h") == 0 || strcmp(args[1], "-?") == 0 || strcmp(args[1], "-help") == 0)) {
+        printf("El cliente se encarga de envíar una consulta al servidor.\nSolo funciona si el servidor está activo.\n");
+        printf("Ejemplo de ejecución: ./cliente dirección_IP config.conf \n");
+        printf("Parámetros: direcció:IP, es la dirección IP del servidor. ");
+        printf("Tambien se puede usar la palabra 'local', en el caso de que el server se encuentre en la misma PC.\n");
+        printf("config.conf, es el archivo que contiene el puerto a conectarse. Tambien puede ser el número.\n");
+        printf("En caso de necesitar ayuda sobre el servidor use el comando ./servidor -h \n");
+        exit(1);
     }
-    return 0;
 }
 
-void mostrarAyuda(){
-    printf("\n Ejemplo de ejecucion: \n ./servidor ./archivoProductos ./config ");
-    printf("\n Ejemplo de ejecucion consulta: \n ./cliente \n");
+void validarParametros(int arg, char *args[]){
+    if(arg != 2 && arg != 3){
+        puts("Ingreso una cantidad incorrecta de parámetros. Deben ser dos o tres");
+        exit(1);
+    }
+    
 }
-int obtenerPuerto(){
-    int puerto=0;
-    FILE*  fp = fopen("config.conf", "r");
-    fscanf(fp, "%d", &puerto);
+
+int esNumero(char *arch_config){
+    char cad_aux[50];
+    strcpy(cad_aux,arch_config);
+    for(int i = 0; i < strlen(cad_aux);i++){
+        if((cad_aux[i] >= 'a' && cad_aux[i] <= 'z' ) || (cad_aux[i] >= 'A' && cad_aux[i] <= 'Z' ))
+            return 0;
+    }
+    return 1;
+}
+
+int obtenerPuerto(char *arch_config){
+    int puerto = 0;
+    FILE*  fp = fopen(arch_config, "r");
+    if(fp)
+        fscanf(fp, "%d", &puerto);
+    else if(esNumero(arch_config))
+        puerto = atoi(arch_config);
+    else{
+        printf("Ingreso un puerto incorrecto\n");
+        exit(1);
+    }
     return puerto;
+}
+
+void obtenerIP(char *ip){
+    char cad_aux[30];
+    strcpy(cad_aux,ip);
+    for(int i = 0; i < 30;i++)
+        cad_aux[i] = toupper(cad_aux[i]);
+    if(strcmp(cad_aux,"LOCAL") == 0)
+        strcpy(ip,"127.0.0.1");
 
 }
 
 int main(int arg, char *args[]) {
-    if (arg == 2 && (strcmp(args[1], "-h") == 0 || strcmp(args[1], "-?") == 0 || strcmp(args[1], "-help") == 0)) {
-        mostrarAyuda();
-        return 0;
-    }
+    
     const char * END_REQUEST = "QUIT";
+    char arch_conf[50];
+    char dir_ip[30];
+    validarParametros(arg,args);
+    mostrarAyuda(arg,args);
+    
+    strcpy(dir_ip,args[1]);
+    strcpy(arch_conf,args[2]); 
+    obtenerIP(dir_ip);
+    
 	struct sockaddr_in direccionServidor;
 	direccionServidor.sin_family = AF_INET;
-	direccionServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
-	direccionServidor.sin_port = htons(obtenerPuerto());
-	int cliente = socket(AF_INET, SOCK_STREAM, 0);
-	if (connect(cliente, (void*) &direccionServidor, sizeof(direccionServidor)) != 0) {
-		perror("No se pudo conectar");
+	direccionServidor.sin_addr.s_addr = inet_addr(dir_ip);
+	direccionServidor.sin_port = htons(obtenerPuerto(arch_conf));
+
+	int socket_server = socket(AF_INET, SOCK_STREAM, 0);
+	if (connect(socket_server, (void*) &direccionServidor, sizeof(direccionServidor)) != 0) {
+		perror("No se pudo conectar al servidor");
 	    return 1;
 	}
     char mensaje[1000];	
@@ -60,18 +101,22 @@ int main(int arg, char *args[]) {
             *aMayuscula = toupper(*aMayuscula);
             aMayuscula++;
         }
-        send(cliente, mensaje, strlen(mensaje), 0);
+        send(socket_server, mensaje, strlen(mensaje), 0);
         printf("Enviando la consulta...\n");
         int received_int = 0;
-        int return_status = read(cliente, &received_int, sizeof(received_int));
+        int return_status = read(socket_server, &received_int, sizeof(received_int));
         char respuesta[ntohl(received_int)];
-        int bytesRecibidos = recv(cliente, respuesta, ntohl(received_int), 0);
+        int bytesRecibidos = recv(socket_server, respuesta, ntohl(received_int), 0);
 		respuesta[bytesRecibidos] = '\0';
-        if(strcmp(mensaje, END_REQUEST) != 0){
+        if(strcmp(mensaje, END_REQUEST) != 0 && strcmp(respuesta,"QUIT") != 0){
             printf("Resultados:\n");
 		    printf("%s\n", respuesta);
         }
+        if(strcmp(respuesta,"QUIT") == 0){
+            printf("Hubo un error con el servidor, se perdió la conección.\n");
+            break;
+        }
 	}while (strcmp(mensaje, END_REQUEST) != 0); 
-
+    close(socket_server);
 	return 0;
 }
--- ejercicio5/servidor.c
+++ ejercicio5/servidor.c
@@ -10,24 +10,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <ctype.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <pthread.h>
+#include <signal.h>
 
 typedef struct{
     char path[50];
     int num_cliente;
 }t_info;
 
-int cliente[10];
+int servidor;
+int cliente[128];
+int clientes_activos[128] = {};
 
-void mostrarAyuda(){
-    printf("\n Ejemplo de ejecucion: \n ./servidor ./archivoProductos ./config ");
-    printf("\n Ejemplo de ejecucion consulta: \n ./cliente \n");
+void controlar_salida(int sig);
+
+void mostrarAyuda(int arg, char *args[]){
+    if (arg == 2 && (strcmp(args[1], "-h") == 0 || strcmp(args[1], "-?") == 0 || strcmp(args[1], "-help") == 0)) {
+        printf("El servidor se encarga de atender las consultas de los clientes.\n");
+        printf("Ejemplo de ejecución: ./servidor articulos.txt config.conf \n");
+        printf("Parámetros: articulos.txt, es el archivo que contiene los artículos a buscar.\n");
+        printf("config.conf, es el archivo que contiene el puerto a conectarse. Tambien puede ser el número.\n");
+        printf("Para cerrar el servidor usar el comando kill -15 [PID server]\n");
+        printf("En caso de necesitar ayuda sobre el cliente use el comando ./cliente -h \n");
+        exit(1);
+    }
 }
 
 void agregarSalida(char out[], char id[], char articulo[], char producto[], char marca[]) {
@@ -48,21 +60,36 @@ int validarParametros(int arg, char *args[]) {
         printf("\nCantidad de parámetros incorrecta. Utilize la ayuda.\n");
         return 1;
     }
-
     struct stat myFile;
     if (stat(args[1], &myFile) < 0) {
         printf("\nNo se encontro el archivo %s\n", args[1]);
         return 1;
     }
-
-    if(arg == 3){
-	    if (stat(args[2], &myFile) < 0) {
-            printf("\nNo se encontro el archivo %s\n", args[2]);
-        return 1;
-        }
-    }
     return 0;
+}
 
+int esNumero(char *arch_config){
+    char cad_aux[50];
+    strcpy(cad_aux,arch_config);
+    for(int i = 0; i < strlen(cad_aux);i++){
+        if((cad_aux[i] >= 'a' && cad_aux[i] <= 'z' ) || (cad_aux[i] >= 'A' && cad_aux[i] <= 'Z' ))
+            return 0;
+    }
+    return 1;
+}
+
+int obtenerPuerto(char *arch_config){
+    int puerto = 0;
+    FILE*  fp = fopen(arch_config, "r");
+    if(fp)
+        fscanf(fp, "%d", &puerto);
+    else if(esNumero(arch_config))
+        puerto = atoi(arch_config);
+    else{
+        printf("Ingreso un puerto incorrecto\n");
+        exit(1);
+    }
+    return puerto;
 }
 
 int obtenerCantidadDeRegistros(char *path) {
@@ -82,22 +109,35 @@ int obtenerCantidadDeRegistros(char *path) {
     return cantfilas;
 }
 
+int buscar_cliente_valido() {
+    int pos = 0;
+    while(clientes_activos[pos] != 0 || pos == 127)
+        pos++;
+    return pos;
+}
+
+int validarPosicion(int i){
+    return clientes_activos[i];
+}
+
 int filtrarArchivo(char *path, char *filtro, int registros, char *salida) {
     FILE *pf;
     int esId = strncmp("ID", filtro, 2);
     int esProducto = strncmp("PRODUCTO", filtro, 8);
     int esMarca = strncmp("MARCA", filtro, 5);
-    int j = 0;
+    int noHayCoincidencias = 0;
+    int noAgrego = 0;
     char id[100];
     char articulo[100];
     char producto[100];
     char marca[100];
     char *igual = strchr(filtro, '=');
     char *buscado = igual;
-
+    
     if(!strchr(filtro, '=')){
         return 1;
     }
+    strcpy(salida,"");
     buscado++;
     pf = fopen(path, "r");
     if (!pf) {
@@ -109,7 +149,6 @@ int filtrarArchivo(char *path, char *filtro, int registros, char *salida) {
     strcpy(producto, " ");
     strcpy(marca, " ");
     while (!feof(pf)) {
-        j++;
         fflush(stdin);
         fscanf(pf, " %[^;]", id);
         fflush(stdin);
@@ -120,11 +159,22 @@ int filtrarArchivo(char *path, char *filtro, int registros, char *salida) {
         fscanf(pf, " ;%[^\r|\n]", marca);
         if (esId == 0 && strcmp(id, buscado) == 0) {
             agregarSalida(salida, id, articulo, producto, marca);
+            noHayCoincidencias = 0;
+            noAgrego = 1;
         } else if (esMarca == 0 && strcmp(marca, buscado) == 0) {
             agregarSalida(salida, id, articulo, producto, marca);
+            noHayCoincidencias = 0;
+            noAgrego = 1;
         } else if (esProducto == 0 && strcmp(producto, buscado) == 0) {
             agregarSalida(salida, id, articulo, producto, marca);
+            noHayCoincidencias = 0;
+            noAgrego = 1;
+        } else if(esId != 0 && esMarca != 0 && esProducto != 0){
+            noHayCoincidencias = 1;
+            noAgrego = 0;
         }
+        if(noAgrego == 0)
+            noHayCoincidencias = 1;
         strcpy(id, " ");
         strcpy(articulo, " ");
         strcpy(producto, " ");
@@ -133,23 +183,15 @@ int filtrarArchivo(char *path, char *filtro, int registros, char *salida) {
 
     strcat(salida, "\0");
     fclose(pf);
-    return 0;
-}
-
-int obtenerPuerto(){
-    int puerto=0;
-    FILE*  fp = fopen("config.conf", "r");
-    fscanf(fp, "%d", &puerto);
-    return puerto;
+    return noHayCoincidencias;
 }
 
 void* atender_cliente(void *info){
     int num = ((t_info*)info)->num_cliente;
-    char path[50];
-    strcpy(path,(char *)info);
+    char path[50]; 
+    strcpy(path,((t_info*)info)->path);
     char *buffer = malloc(1000);
     while (1) {
-
 		int bytesRecibidos = recv(cliente[num], buffer, 1000, 0);
 		if (bytesRecibidos <= 0) {
 			continue;
@@ -162,7 +204,6 @@ void* atender_cliente(void *info){
         }
         int registros = obtenerCantidadDeRegistros(path);
         char salida[registros * 40];
-
         int res = filtrarArchivo(path, buffer, registros, salida);
         if(res == 1){
             strcpy(salida,"No se encontraron resultados.");
@@ -177,48 +218,74 @@ void* atender_cliente(void *info){
 		send(cliente[num], salida_, sizeof(salida_), 0);
 	}
     free(buffer);
+    clientes_activos[num] = 0;
 }
 
 int main(int arg,char *args[]) {
-	pid_t server_demonio;
-    if (arg == 2 && (strcmp(args[1], "-h") == 0 || strcmp(args[1], "-?") == 0 || strcmp(args[1], "-help") == 0)) {
-        mostrarAyuda();
-        return 0;
-    }
+	mostrarAyuda(arg,args);
+    validarParametros(arg,args);
+
+    char arch_conf[50];
+    pid_t server_demonio;
+    strcpy(arch_conf,args[2]);
 
 	struct sockaddr_in direccionServidor;
 	direccionServidor.sin_family = AF_INET;
 	direccionServidor.sin_addr.s_addr = INADDR_ANY;
-	direccionServidor.sin_port = htons(obtenerPuerto());
-
+	direccionServidor.sin_port = htons(obtenerPuerto(arch_conf));
+    
     server_demonio = fork();
     if(server_demonio > 0)
         exit(0);
-
-	int servidor = socket(AF_INET, SOCK_STREAM, 0);
+    signal(SIGINT, controlar_salida);
+    signal(SIGTERM, controlar_salida);
+    
+    
+	servidor = socket(AF_INET, SOCK_STREAM, 0);
 	int activado = 1;
 	setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
 	if (bind(servidor, (void*) &direccionServidor, sizeof(direccionServidor)) != 0) {
 		perror("Falló el bind");
 		return 1;
 	}
-
+    
 	listen(servidor, SOMAXCONN);
 	pthread_t hilo;
-    int i = 0;
-    t_info info[10];
+    int pos;
+    t_info info[128];
 
 	struct sockaddr_in direccionCliente;
 	unsigned int tamanioDireccion;
     while(1){
-        strcpy(info[i].path,args[1]);
-        info[i].num_cliente = i;
-        cliente[i] = accept(servidor, (void*) &direccionCliente, &tamanioDireccion);
-        pthread_create(&hilo,NULL, atender_cliente,&info[i]);
+        pos = buscar_cliente_valido();
+        strcpy(info[pos].path,args[1]);
+        info[pos].num_cliente = pos;
+        cliente[pos] = accept(servidor, (void*) &direccionCliente, &tamanioDireccion);
+        clientes_activos[pos] = 1;
+        pthread_create(&hilo,NULL, atender_cliente,&info[pos]);
         printf("Bienvenido cliente\n");
+    }
+	return 0;
+}
+
+void controlar_salida(int signal) {
+    int i = 0;
+    char cadena_salida[5] = "QUIT";
+    int number_to_send;
+	int converted_number;
+    number_to_send = strlen(cadena_salida);
+    converted_number = htonl(number_to_send);
+    char salida_[number_to_send];
+    printf("Cerrando el servidor...\n");
+    while(i<128){
+        if(validarPosicion(i) == 1){
+	        strcpy(salida_,cadena_salida);
+	        write(cliente[i], &converted_number, sizeof(converted_number));
+	        send(cliente[i], salida_, sizeof(salida_), 0);
+            close(cliente[i]);
+        }
         i++;
     }
-
-    //despues del accept se crea el hilos
-	return 0;
+    close(servidor);
+    exit(1);
 }
